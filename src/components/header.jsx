049168c2af33c865b1124473cd3232b768a694f7
Akshat Bhatt <akshat.bhatt.ece23@itbhu.ac.in>
2025-03-06 02:42:18 +0530

    Modified line graph plot from month based to transaction based

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,21 +10,21 @@ import Hamburger from "./Hamburger"
 
 const Header = () => {
   const [user, loading] = useAuthState(auth);
-  const [isHovered, setIsHovered]=useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!loading && !user) {
       navigate("/");
     }
-  }, [user, loading, navigate]); // Added navigate to dependencies
+  }, [user, loading, navigate]);
 
   function logoutFunction() {
     signOut(auth)
       .then(() => {
         toast.success("Logged Out Successfully!");
         localStorage.clear();
-        navigate("/"); // Ensure redirection happens after logout
+        navigate("/");
       })
       .catch((error) => {
         toast.error(error.message);
@@ -34,23 +34,22 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between w-full py-4 px-6 bg-gray-800 text-white">
       <div className="navbar font-medium text-[2rem]">
-        {user && <Hamburger isHovered={isHovered} setIsHovered={setIsHovered}/>}
+        {user && <Hamburger isHovered={isHovered} setIsHovered={setIsHovered} />}
         Tracky
       </div>
       {user && (
-        <div
-          className="logout flex items-center text-3xl"
-          onClick={logoutFunction}
-        >
-          <img
-            src={user.photoURL ? user.photoURL : userSvg}
-            width={user.photoURL ? "32" : "24"}
-            height={"32"}
-            style={{marginRight: 10}}
-            // alt="User Profile"
-          />
-          Logout
-        </div>
+          <div
+            className="logout flex items-center text-3xl"
+            onClick={logoutFunction}
+          >
+            <img
+              src={user.photoURL ? user.photoURL : userSvg}
+              width={user.photoURL ? "32" : "24"}
+              height={"32"}
+              style={{ marginRight: 10 }}
+            />
+            Logout
+          </div>
       )}
     </div>
   );
